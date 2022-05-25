d8dc9a0eac9b73f112e77e8475b7fd2676bf9a9e
Lucas Braga <802.266@alunos.unigran.br>
2022-05-24 20:57:18 -0400

    Implementa builder para CargoPapel

---
+++ src/main/java/br/unigran/restaurante/app/builder/CargoPapelBuilder.java
@@ -0,0 +1,22 @@
+package br.unigran.restaurante.app.builder;
+
+import br.unigran.restaurante.app.models.Cargo;
+import br.unigran.restaurante.app.models.CargoPapel;
+import br.unigran.restaurante.app.models.Papel;
+
+public class CargoPapelBuilder {
+    private Papel papel;
+    private Cargo cargo;
+
+    public CargoPapelBuilder(Papel papel, Cargo cargo) {
+        this.papel = papel; 
+        this.cargo = cargo;
+    }
+
+    public CargoPapel build() throws Exception {
+        CargoPapel cargoPapel = new CargoPapel();
+        cargoPapel.setCargo(cargo);
+        cargoPapel.setPapel(papel);
+        return cargoPapel;
+    }
+}
