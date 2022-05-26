3b29151224d52ff0dabd392a533187a3e35d752b
Lucas Braga <lucasfbraga20@gmail.com>
2022-05-25 23:39:04 -0400

    Implementa DAO genérico

---
+++ src/main/java/br/unigran/restaurante/app/persistence/DAO.java
@@ -0,0 +1,81 @@
+package br.unigran.restaurante.app.persistence;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+public class DAO<T> {
+    public T salvar(T objeto, Class<T> classe, Integer id) throws Exception {
+        EntityManager em = SingletonDao.getInstance().getEM();
+        EntityTransaction etx = SingletonDao.getInstance().getTX();
+
+        try {
+            etx.begin();
+            if (id == null) {
+                em.persist(objeto);
+            } else {
+                if (!em.contains(objeto)) {
+                    if (em.find(classe, id) == null) {
+                        throw new Exception("Erro ao atualizar: " + classe.getSimpleName());
+                    }
+                }
+                objeto = em.merge(objeto);
+            }
+            etx.commit();
+        } catch(Exception e) {
+            etx.rollback();
+            throw e;
+        }
+        
+        return objeto;
+    }
+
+    public void remover(Integer id, Class<T> classe) throws Exception {
+        EntityManager em = SingletonDao.getInstance().getEM();
+        EntityTransaction etx = SingletonDao.getInstance().getTX();
+
+        try {
+            T objeto = em.find(classe, id);
+            if (objeto == null) {
+                throw new Exception("Não foi possível remover um " + classe.getSimpleName() + " com esse id.");
+            }
+            etx.begin();
+            em.remove(objeto);
+            etx.commit();
+        } catch (Exception e) {
+            etx.rollback();
+            throw e;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<T> listarTodos(Class<T> classe) throws Exception {
+        EntityManager em = SingletonDao.getInstance().getEM();
+        List<T> objetos = null;
+
+        try {
+            objetos = em.createQuery("FROM " + classe.getSimpleName() + " o ").getResultList();
+        } catch (Exception e) {
+            throw e;
+        }
+
+        return objetos;
+    }
+
+    public T consultarPorId(Integer id, Class<T> classe) throws Exception {
+        EntityManager em = SingletonDao.getInstance().getEM();
+        T objeto = null;
+
+        try {
+            objeto = em.find(classe, id);
+            if (objeto == null) {
+                throw new Exception("Não foi possível achar um " + classe.getSimpleName() + " com esse id.");
+            }
+        } catch (Exception e) {
+            throw e;
+        }
+
+        return objeto;
+    }
+}
--- src/main/java/br/unigran/restaurante/app/repository/Teste.java
+++ src/main/java/br/unigran/restaurante/app/repository/Teste.java
@@ -1,7 +1,91 @@
 package br.unigran.restaurante.app.repository;
 
+import br.unigran.restaurante.app.persistence.DAO;
+import java.util.List;
+import br.unigran.restaurante.app.models.Cargo;
+
 public class Teste {
     public static void main(String[] args) {
-        System.out.println("teste");
+        /*
+            CRIAÇÃO GENÉRICO
+
+        Cargo cargoCriado = new Cargo();
+        cargoCriado.setDescricao("Teste dao generico");
+
+        DAO<Cargo> cargoDAO = new DAO<Cargo>();
+
+        try {
+            cargoDAO.salvar(cargoCriado, Cargo.class, cargoCriado.getId());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        */
+
+
+
+        /*
+            CONSULTA GENÉRICO
+
+        DAO<Cargo> cargoDAO = new DAO<Cargo>();
+        try {
+            Cargo cargoConsultado = cargoDAO.consultarPorId(801, Cargo.class);
+            System.out.println(cargoConsultado.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        */
+
+
+
+        /*
+            CONSULTA TODOS
+
+        DAO<Cargo> cargoDAO = new DAO<Cargo>();
+        try {
+            List<Cargo> cargosConsultados = cargoDAO.listarTodos(Cargo.class);
+            for (int i = 0; i < cargosConsultados.size(); i++) {
+                System.out.println(cargosConsultados.get(i).toString());
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        */
+
+
+
+        /*
+            ATUALIZAR
+
+        DAO<Cargo> cargoDAO = new DAO<Cargo>();
+        Cargo cargoConsultado;
+        try {
+            cargoConsultado = cargoDAO.consultarPorId(801, Cargo.class);
+            System.out.println(cargoConsultado.toString());
+
+            cargoConsultado.setDescricao("Descrição DAO atualizado");
+
+            cargoConsultado = cargoDAO.salvar(cargoConsultado, Cargo.class, cargoConsultado.getId());
+
+            System.out.println(cargoConsultado.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        */
+
+
+
+        /*
+            DELETAR
+        DAO<Cargo> cargoDAO = new DAO<Cargo>();
+        Cargo cargoConsultado;
+        try {
+            cargoConsultado = cargoDAO.consultarPorId(801, Cargo.class);
+            System.out.println(cargoConsultado.toString());
+
+            cargoDAO.remover(cargoConsultado.getId(), Cargo.class);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        */
     }
 }
