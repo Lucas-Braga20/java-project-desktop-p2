ff9138e7252b9d1af6967f0cf262deed80c62bd9
Willian Matiussi <willianmatiussi23@gmail.com>
2022-05-26 19:33:32 -0400

    IngredienteUC, PratoUC, ProdutoUC

--- src/main/java/br/unigran/restaurante/app/casouso/ProdutoUC.java
+++ src/main/java/br/unigran/restaurante/app/casouso/ProdutoUC.java
@@ -4,11 +4,12 @@ import java.util.List;
 
 import br.unigran.restaurante.app.builder.ProdutoBuilder;
 import br.unigran.restaurante.app.models.Produto;
-import br.unigran.restaurante.app.persistence.ProdutoDAO;
+import br.unigran.restaurante.app.persistence.DAO;
 
 public class ProdutoUC {
     public Produto salvar(ProdutoBuilder produtoBuilder) throws Exception {
-        Produto produto = new ProdutoDAO().salvar(produtoBuilder.build());
+        Produto produto = produtoBuilder.build();
+        produto = new DAO<Produto>().salvar(produto, Produto.class, produto.getId().intValue());
 
         System.out.println(produto.toString());
 
@@ -16,19 +17,21 @@ public class ProdutoUC {
     }
 
     public Produto atualizar(ProdutoBuilder produtoBuilder, Produto produto) throws Exception {
-        Produto produtoAtualizado = new ProdutoDAO().salvar(produtoBuilder.build(produto));
+        Produto produtoAnterior = produtoBuilder.build(produto);
+        Produto produtoAtualizado = new DAO<Produto>().salvar(produtoAnterior, Produto.class,
+                produtoAnterior.getId().intValue());
 
         System.out.println(produtoAtualizado.toString());
 
         return produtoAtualizado;
     }
 
-    public void remover(Long id) throws Exception {
-        new ProdutoDAO().remover(id);
+    public void remover(Integer id) throws Exception {
+        new DAO<Produto>().remover(id, Produto.class);
     }
 
     public List<Produto> listarTodos() throws Exception {
-        List<Produto> produtos = new ProdutoDAO().listarTodos();
+        List<Produto> produtos = new DAO<Produto>().listarTodos(Produto.class);
 
         for (int i = 0; i < produtos.size(); i++) {
             System.out.println(produtos.get(i).toString());
@@ -37,8 +40,8 @@ public class ProdutoUC {
         return produtos;
     }
 
-    public Produto consultarPorId(Long id) throws Exception {
-        Produto produto = new ProdutoDAO().consultarPorId(id);
+    public Produto consultarPorId(Integer id) throws Exception {
+        Produto produto = new DAO<Produto>().consultarPorId(id, Produto.class);
 
         System.out.println(produto.toString());
 
