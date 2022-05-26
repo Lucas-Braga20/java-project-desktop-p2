518e6e21ba6eae0ceba106f63c8200d9b814301d
Willian Matiussi <willianmatiussi23@gmail.com>
2022-05-26 19:20:35 -0400

    MesaUC

--- src/main/java/br/unigran/restaurante/app/casouso/MesaUC.java
+++ src/main/java/br/unigran/restaurante/app/casouso/MesaUC.java
@@ -4,44 +4,46 @@ import java.util.List;
 
 import br.unigran.restaurante.app.builder.MesaBuilder;
 import br.unigran.restaurante.app.models.Mesa;
-import br.unigran.restaurante.app.persistence.MesaDAO;
+import br.unigran.restaurante.app.persistence.DAO;
 
 public class MesaUC {
-    public Mesa salvar(MesaBuilder mesaBuilder) throws Exception {
-        Mesa mesa = new MesaDAO().salvar(mesaBuilder.build());
+  public Mesa salvar(MesaBuilder mesaBuilder) throws Exception {
+    Mesa mesa = mesaBuilder.build();
+    mesa = new DAO<Mesa>().salvar(mesa, Mesa.class, mesa.getId().intValue());
 
-        System.out.println(mesa.toString());
+    System.out.println(mesa.toString());
 
-        return mesa;
+    return mesa;
+  }
+
+  public Mesa atualizar(MesaBuilder mesaBuilder, Mesa mesa) throws Exception {
+    Mesa mesaAnterior = mesaBuilder.build(mesa);
+    Mesa mesaAtualizado = new DAO<Mesa>().salvar(mesaAnterior, Mesa.class, mesaAnterior.getId().intValue());
+
+    System.out.println(mesaAtualizado.toString());
+
+    return mesaAtualizado;
+  }
+
+  public void remover(Integer id) throws Exception {
+    new DAO<Mesa>().remover(id, Mesa.class);
+  }
+
+  public List<Mesa> listarTodos() throws Exception {
+    List<Mesa> mesas = new DAO<Mesa>().listarTodos(Mesa.class);
+
+    for (int i = 0; i < mesas.size(); i++) {
+      System.out.println(mesas.get(i).toString());
     }
 
-    public Mesa atualizar(MesaBuilder mesaBuilder, Mesa mesa) throws Exception {
-        Mesa mesaAtualizado = new MesaDAO().salvar(mesaBuilder.build(mesa));
+    return mesas;
+  }
 
-        System.out.println(mesaAtualizado.toString());
+  public Mesa consultarPorId(Integer id) throws Exception {
+    Mesa mesa = new DAO<Mesa>().consultarPorId(id, Mesa.class);
 
-        return mesaAtualizado;
-    }
+    System.out.println(mesa.toString());
 
-    public void remover(Integer id) throws Exception {
-        new MesaDAO().remover(id);
-    }
-
-    public List<Mesa> listarTodos() throws Exception {
-        List<Mesa> mesas = new MesaDAO().listarTodos();
-
-        for (int i = 0; i < mesas.size(); i++) {
-            System.out.println(mesas.get(i).toString());
-        }
-
-        return mesas;
-    }
-
-    public Mesa consultarPorId(Integer id) throws Exception {
-        Mesa mesa = new MesaDAO().consultarPorId(id);
-
-        System.out.println(mesa.toString());
-
-        return mesa;
-    }
+    return mesa;
+  }
 }
